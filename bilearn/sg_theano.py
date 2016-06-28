c38dd9a4c867642a41eeb97e32ac0464e39cc043
Vlad Niculae <vlad@vene.ro>
2016-06-28 15:20:01 -0400

    refactor theano sg for regression

--- bilearn/sg_theano.py
+++ bilearn/sg_theano.py
@@ -21,6 +21,7 @@ from sklearn.utils.extmath import safe_sparse_dot
 import theano as tn
 import theano.tensor as T
 from theano import sparse as tsp
+from theano.sparse.basic import _is_sparse_variable as _tn_is_sparse
 
 from lasagne.updates import adam  # could use sgd, adagrad, etc
 from lasagne.objectives import binary_hinge_loss, binary_crossentropy
@@ -33,22 +34,63 @@ def safe_sparse_mul(X, Y):
         return X * Y
 
 
-class LowRankBilinear(object):
+def theano_safe_sparse_dot(X, Y):
+    if _tn_is_sparse(X) or _tn_is_sparse(Y):
+        return tsp.dot(X, Y)
+    else:
+        return T.dot(X, Y)
 
-    def __init__(self, n_components=5, l2_reg=0.01, l2_reg_linear=0.01,
-                 l2_reg_diag=0.01, max_iter=1000, random_state=0,
-                 warm_start=False, fit_diag=True, fit_linear=True,
-                 loss_func=None, update_rule=None, update_params=None):
+
+class BilinearSG(object):
+
+    def _get_low_rank_term(self, X_left, X_right, U_init, V_init):
+        U = tn.shared(value=U_init, name='U')
+        V = tn.shared(value=V_init, name='V')
+
+        UX_left = theano_safe_sparse_dot(X_left, U)
+        VX_right = theano_safe_sparse_dot(X_right, V)
+
+        y_pred = T.batched_dot(UX_left, VX_right)
+
+        return y_pred, [U, V]
+
+    def _get_linear_term(self, X_left, X_right, w_left_init, w_right_init):
+        n_features_left = X_left.shape[1]
+        n_features_right = X_right.shape[1]
+        w_left = tn.shared(value=w_left_init, name='w_left')
+        w_right = tn.shared(value=w_right_init, name='w_right')
+
+        wX_left = theano_safe_sparse_dot(X_left, w_left)
+        wX_right = theano_safe_sparse_dot(X_right, w_right)
+        y_pred = wX_left + wX_right
+
+        return y_pred, [w_left, w_right]
+
+    def _get_diagonal_term(self, X_left, X_right, diag_init):
+        diag = tn.shared(value=diag_init, name='diag')
+
+        if _tn_is_sparse(X_left) or _tn_is_sparse(X_right):
+            XlXr = tsp.mul(X_left, X_right)
+            y_pred = tsp.dot(XlXr, diag)
+        else:
+            XlXr = T.mul(X_left, X_right)
+            y_pred = T.dot(XlXr, diag)
+
+        return y_pred, [diag]
+
+
+class BilinearRegressorSG(BilinearSG):
+
+    def __init__(self, n_components=5, l2_reg=0.01, max_iter=1000,
+                 random_state=0, warm_start=False, fit_diag=True,
+                 fit_linear=True, update_rule=None, update_params=None):
         self.n_components = n_components
         self.l2_reg = l2_reg
-        self.l2_reg_linear = l2_reg_linear
-        self.l2_reg_diag = l2_reg_diag
         self.max_iter = max_iter
         self.random_state = random_state
         self.warm_start = warm_start
         self.fit_diag = fit_diag
         self.fit_linear = fit_linear
-        self.loss_func = loss_func
         self.update_rule = update_rule
         self.update_params = update_params
 
@@ -60,14 +102,13 @@ class LowRankBilinear(object):
         n_samples_right, n_features_right = X_right.shape
 
         assert n_samples == n_samples_right
-
-        fit_diag = self.fit_diag
-        if fit_diag and n_features_left != n_features_right:
-            warnings.warn("Cannot fit diagonal if spaces have diff. dim.")
-            fit_diag = False
+        if self.fit_diag and n_features_left != n_features_right:
+            raise ValueError("Cannot fit diagonal term if spaces have "
+                             "different number of features.")
 
         rng = check_random_state(self.random_state)
 
+        # initialize all params if warm start is on
         if self.warm_start and hasattr(self, 'U_'):
             U_init = self.U_
         else:
@@ -77,6 +118,21 @@ class LowRankBilinear(object):
             V_init = self.V_
         else:
             V_init = rng.randn(n_features_right, self.n_components)
+
+        if self.warm_start and hasattr(self, 'w_left_'):
+            w_left_init = self.w_left_
+        else:
+            w_left_init = np.zeros(n_features_left)
+
+        if self.warm_start and hasattr(self, 'w_right_'):
+            w_right_init = self.w_right_
+        else:
+            w_right_init = np.zeros(n_features_right)
+
+        if self.warm_start and hasattr(self, 'diag_'):
+            diag_init = self.diag_
+        else:
+            diag_init = np.ones(n_features_left)
 
         if self.update_rule is None:
             update_rule = adam
@@ -88,84 +144,57 @@ class LowRankBilinear(object):
         else:
             update_params = self.update_params
 
-        U = tn.shared(value=U_init, name='U')
-        V = tn.shared(value=V_init, name='V')
-        trainable_params = [U, V]
-
-        if self.fit_linear:
-            w_left = tn.shared(value=np.zeros(n_features_left), name='w_left')
-            w_right = tn.shared(value=np.zeros(n_features_right),
-                                name='w_right')
-            trainable_params.extend([w_left, w_right])
-
-        if fit_diag:
-            diag = tn.shared(value=np.ones(n_features_left), name='diag')
-            trainable_params.append(diag)
-
         if sp.issparse(X_left):
             X_left_tn = tsp.csr_matrix('X_left_tn')
-            UX_left = tsp.dot(X_left_tn, U)
         else:
             X_left_tn = T.dmatrix('X_left_tn')
-            UX_left = T.dot(X_left_tn, U)
 
-        if sp.issparse(X_left):
+        if sp.issparse(X_right):
             X_right_tn = tsp.csr_matrix('X_right_tn')
-            UX_right = tsp.dot(X_right_tn, V)
         else:
             X_right_tn = T.dmatrix('X_right_tn')
-            UX_right = T.dot(X_right_tn, V)
 
-        y_tn = T.bvector('y')
+        y_tn = T.dvector('y')
 
-        y_scores = T.batched_dot(UX_left, UX_right)
+        y_pred, vars = self._get_low_rank_term(X_left_tn, X_right_tn,
+                                               U_init, V_init)
+        U, V = vars
 
         if self.fit_linear:
-            if sp.issparse(X_left_tn):
-                wX_left = tsp.dot(X_left_tn, w_left)
-            else:
-                wX_left = T.dot(X_left_tn, w_left)
+            y_linear, vars_linear = self._get_linear_term(
+                X_left_tn, X_right_tn, w_left_init, w_right_init)
 
-            if sp.issparse(X_right_tn):
-                wX_right = tsp.dot(X_right_tn, w_right)
-            else:
-                wX_right = T.dot(X_right_tn, w_right)
+            y_pred += y_linear
+            vars += vars_linear
+            w_left, w_right = vars_linear
 
-            y_scores += wX_left + wX_right
+        if self.fit_diag:
+            y_diag, vars_diag = self._get_diagonal_term(
+                X_left_tn, X_right_tn, diag_init)
 
-        if fit_diag:
-            if sp.issparse(X_left_tn) or sp.issparse(X_right_tn):
-                XlXr = tsp.mul(X_left_tn, X_right_tn)
-                y_scores += tsp.dot(XlXr, diag)
-            else:
-                XlXr = T.mul(X_left_tn, X_right_tn)
-                y_scores += T.dot(XlXr, diag)
+            y_pred += y_diag
+            vars += vars_diag
+            diag, = vars_diag
+
+        # squared loss
+        loss = T.mean((y_pred - y_tn) ** 2)
 
         # hinge loss
-        loss = binary_hinge_loss(y_scores, y_tn).mean()
+        # loss = binary_hinge_loss(y_scores, y_tn).mean()
 
         # regularization
-        loss += self.l2_reg * (T.sum(U ** 2) + T.sum(V ** 2))
+        for var in vars:
+            loss += self.l2_reg * T.sum(var ** 2)
 
-        if self.fit_linear:
-            loss += self.l2_reg_linear * (T.sum(w_left ** 2) +
-                                          T.sum(w_right ** 2))
-
-        if fit_diag:
-            loss += self.l2_reg_diag * T.sum(diag ** 2)
-
-        train_model = tn.function(inputs=[],
+        train_model = tn.function(inputs=[X_left_tn, X_right_tn, y_tn],
                                   outputs=loss,
-                                  updates=update_rule(loss,
-                                                      trainable_params,
-                                                      **update_params),
-                                  givens={X_left_tn: X_left,
-                                          X_right_tn: X_right,
-                                          y_tn: y})
+                                  updates=update_rule(loss, vars,
+                                                      **update_params))
 
         self.losses_ = []
+
         for _ in range(self.max_iter):
-            self.losses_.append(train_model())
+            self.losses_.append(train_model(X_left, X_right, y))
 
         self.U_ = U.eval()
         self.V_ = V.eval()
@@ -173,13 +202,17 @@ class LowRankBilinear(object):
         if self.fit_linear:
             self.w_left_ = w_left.eval()
             self.w_right_ = w_right.eval()
+
         if fit_diag:
             self.diag_ = diag.eval()
 
-    def decision_function(self, X_left, X_right):
+        return self
+
+    def predict(self, X_left, X_right):
         y_pred = np.sum(safe_sparse_dot(X_left, self.U_) *
                         safe_sparse_dot(X_right, self.V_),
                         axis=1)
+
         if self.fit_linear:
             y_pred += safe_sparse_dot(X_left, self.w_left_)
             y_pred += safe_sparse_dot(X_right, self.w_right_)
@@ -188,9 +221,6 @@ class LowRankBilinear(object):
             y_pred += safe_sparse_dot(safe_sparse_mul(X_left, X_right),
                                       self.diag_)
         return y_pred
-
-    def predict(self, X_left, X_right):
-        return self.decision_function(X_left, X_right) > 0
 
 
 if __name__ == '__main__':
@@ -204,7 +234,7 @@ if __name__ == '__main__':
 
     y = np.sum(np.dot(X_left, true_U) * np.dot(X_right, true_V), axis=1)
     y += np.dot((X_left * X_right), true_diag)
-    y = (y > 0).astype(np.int8)
+    # y += 0.01 * rng.randn(100)
 
     from time import time
     from itertools import product
@@ -212,17 +242,17 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     for fit_linear, fit_diag in product((False, True), (False, True)):
+
         print("fit_linear={}, fit_diag={}".format(fit_linear, fit_diag))
-        lrbl = LowRankBilinear(n_components=5,
-                               l2_reg=0.001,
-                               l2_reg_linear=0.001,
-                               l2_reg_diag=0.001,
-                               fit_linear=fit_linear, fit_diag=fit_diag,
-                               update_params={'learning_rate': 0.01},
-                               # max_iter=10000,
-                               random_state=0)
+
+        lrbl = BilinearRegressorSG(n_components=10,
+                                   l2_reg=0.01,
+                                   fit_linear=fit_linear,
+                                   fit_diag=fit_diag,
+                                   max_iter=20000,
+                                   random_state=0)
         t0 = time()
-        # lrbl.fit(sp.csr_matrix(X_left), sp.csr_matrix(X_right), y)
+
         lrbl.fit(X_left, X_right, y)
         t0 = time() - t0
         y_pred_train = lrbl.predict(X_left, X_right)
@@ -234,15 +264,14 @@ if __name__ == '__main__':
 
         y_val += np.dot((X_left_val * X_right_val), true_diag)
 
-        y_val = y_val > 0
         y_pred = lrbl.predict(X_left_val, X_right_val)
 
-        plt.semilogy(lrbl.losses_,label="fit_linear={}, fit_diag={}".format(
+        plt.semilogy(lrbl.losses_, label="fit_linear={}, fit_diag={}".format(
            fit_linear, fit_diag))
 
         print("\t{:.2f}s".format(t0))
-        print("\tTrain accuracy: {:.2f}".format(np.mean(y_pred_train == y)))
-        print("\tTest accuracy: {:.2f}".format(np.mean(y_pred == y_val)))
+        print("\tTrain MSE: {:.5f}".format(np.mean((y_pred_train - y) ** 2)))
+        print("\tTest MSE: {:.5f}".format(np.mean((y_pred - y_val) ** 2)))
 
     plt.legend()
     plt.show()
