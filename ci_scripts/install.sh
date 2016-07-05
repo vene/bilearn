e78ae61e9d36777cde709228ff1f644ba91abed6
Vlad Niculae <vlad@vene.ro>
2016-07-05 12:34:17 -0400

    install sklearn via pip instead

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -25,11 +25,11 @@ popd
 # Configure the conda environment and put it in the path using the
 # provided versions
 conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
-      numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION cython=$CYTHON_VERSION \
-      scikit-learn=$SKLEARN_VERSION
+      numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION cython=$CYTHON_VERSION
 
 source activate testenv
 
+pip install scikit-learn==$SKLEARN_VERSION
 conda install -y -q -c conda-forge sklearn-contrib-lightning
 
 if [[ "$COVERAGE" == "true" ]]; then
